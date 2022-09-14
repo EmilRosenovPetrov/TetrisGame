8aa8bd1f2a4ae6de2cfc7b1a0cea0b4833ae5569
Emil Petrov <emil.petrov2020@gmail.com>
2022-09-14 16:37:00 +0300

    GameState added

---
+++ TetrisGame/GameState.cs
@@ -0,0 +1,41 @@
+﻿namespace TetrisGame
+{
+    internal class GameState
+    {
+        private Block currentBlock;
+
+        public Block CurrentBlock
+        {
+            get => currentBlock;
+            private set
+            {
+                currentBlock = value;
+                currentBlock.Reset();
+            }
+        }
+
+        public GameGrid GameGrid { get; }
+        public BlockQueue BlockQueue { get; }
+        public bool GameOver { get; private set; }
+
+        public GameState()
+        {
+            GameGrid = new GameGrid(22, 10);
+            BlockQueue = new BlockQueue();
+            CurrentBlock = BlockQueue.GetAndupdate();
+        }
+
+        private bool BlockFits()
+        {
+            foreach (Position p in CurrentBlock.TilePositions())
+            {
+                if (!GameGrid.IsEmpty(p.Row, p.Column))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
