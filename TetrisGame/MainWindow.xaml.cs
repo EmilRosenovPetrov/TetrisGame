3aa6e5816636b039aaf1ba47a25192f5b7ba3392
Emil Petrov <emil.petrov2020@gmail.com>
2022-09-15 21:21:59 +0300

    Custom delay added

--- TetrisGame/MainWindow.xaml.cs
+++ TetrisGame/MainWindow.xaml.cs
@@ -39,6 +39,10 @@
 
         private readonly Image[,] imageControls;
 
+        private readonly int maxDelay = 1000;
+        private readonly int minDelay = 75;
+        private readonly int delayDecrease = 25;
+
         private GameState gameState = new GameState();
 
         public MainWindow()
@@ -138,7 +142,8 @@
 
             while (!gameState.GameOver)
             {
-                await Task.Delay(500);
+                int delay = Math.Max(minDelay, maxDelay - (gameState.Score*delayDecrease));
+                await Task.Delay(delay);
                 gameState.MoveBlockDown();
                 Draw(gameState);
             }
